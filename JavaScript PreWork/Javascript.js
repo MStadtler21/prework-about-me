e239763085b5741f520413373f8d938d83850f91
MatStat <powdersteamboat@gmail.com>
2020-06-13 20:21:22 -0600

    Add functionality to all buttons in HTML

---
+++ JavaScript PreWork/Javascript.js
@@ -0,0 +1,18 @@
+
+
+function grow() {
+    document.getElementById("box").style.height = "300px"
+    document.getElementById("box").style.width = "300px"
+}
+
+function blue() {
+    document.getElementById("box").style.backgroundColor = "blue"
+}
+
+function fade() {
+    document.getElementById("box").style.opacity = "0.5"
+}
+
+function reset() {
+    document.getElementById("box").style = "height:150px; width:150px; background-color:orange; margin:25px; opacity: 1;"
+}
